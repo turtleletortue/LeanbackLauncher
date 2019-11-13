794d5e353eae221e4c772a4a12f85ad1abac7733
Black-Seraph <admin@black-seraph.com>
2019-11-13 21:35:12 +0100

    Recommendations & application blacklist removed (both are broken anyway)

--- app/src/main/java/com/amazon/tv/leanbacklauncher/settings/LegacyHomeScreenPreferenceFragment.java
+++ app/src/main/java/com/amazon/tv/leanbacklauncher/settings/LegacyHomeScreenPreferenceFragment.java
@@ -34,9 +34,9 @@ public class LegacyHomeScreenPreferenceFragment extends GuidedStepSupportFragmen
             if (blacklistCount != -1) {
                 description = getResources().getQuantityString(R.plurals.recommendation_blacklist_action_description, blacklistCount, new Object[]{Integer.valueOf(blacklistCount)});
             }
-            actions.add(new Builder(getActivity()).id(1).title((int) R.string.recommendation_blacklist_action_title).description((CharSequence) description).build());
+            // actions.add(new Builder(getActivity()).id(1).title((int) R.string.recommendation_blacklist_action_title).description((CharSequence) description).build());
             actions.add(new Builder(getActivity()).id(2).title((int) R.string.home_screen_order_action_title).build());
-            actions.add(new Builder(getActivity()).id(3).title(R.string.hidden_applications_title).build());
+            // actions.add(new Builder(getActivity()).id(3).title(R.string.hidden_applications_title).build());
             setActions(actions);
         }
     }
